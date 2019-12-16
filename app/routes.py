6d3725c85a67526a00dcb020e3dc8856d5a1bce7
rthorst <thorstadrs@gmail.com>
2019-12-15 21:14:09 -0500

    initial addition of flask-freeze and build directory

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for
 from app import app
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index/")
 
 def index():
     return render_template("index.html")
